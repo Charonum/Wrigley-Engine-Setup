8ee9cbcc77a0ecb075fe692d7f255ff624fe535a
Charonum <78572976+Charonum@users.noreply.github.com>
2021-07-05 10:23:51 -0700

    Add files via upload

--- Installer.py
+++ Installer.py
@@ -1,24 +1,14 @@
+import getpass
 import sys
 import subprocess
 import os
 import zipfile
 import shutil
-import ctypes
 import wget
-
-ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
-
-if not ctypes.windll.shell32.IsUserAnAdmin():
-    import sys
-    ctypes.windll.shell32.ShellExecuteW(
-        None, 'runas', sys.executable, ' '.join(sys.argv), None, None)
-    exit(0)
-else:
-    pass
 
 
 try:
-    os.mkdir(rf"C:\Program Files\Charonum\Wrigley Engine")
+    os.mkdir(rf"C:\Users\{getpass.getuser()}\Charonum\Wrigley Engine")
 except:
     pass
 
@@ -26,13 +16,13 @@ subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
 subprocess.call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 
 url = "https://github.com/Charonum/WE_Startup/archive/refs/heads/main.zip"
-wget.download(url, rf"C:\Program Files\Charonum\Wrigley Engine")
+wget.download(url, rf"C:\Users\{getpass.getuser()}\Charonum\Wrigley Engine")
 
-with zipfile.ZipFile(rf"C:\Program Files\Charonum\Wrigley Engine\WE_Startup-main.zip", 'r') as zipObj:
-    zipObj.extractall(rf"C:\Program Files\Charonum\Wrigley Engine")
+with zipfile.ZipFile(rf"C:\Users\{getpass.getuser()}\Charonum\Wrigley Engine\WE_Startup-main.zip", 'r') as zipObj:
+    zipObj.extractall(rf"C:\Users\{getpass.getuser()}\Charonum\Wrigley Engine")
 
-for file in os.listdir(rf"C:\Program Files\Charonum\Wrigley Engine\WE_Startup-main"):
-    shutil.move(rf"C:\Program Files\Charonum\Wrigley Engine\WE_Startup-main\{file}", rf"C:\Program Files\Charonum\Wrigley Engine")
+for file in os.listdir(rf"C:\Users\{getpass.getuser()}\Charonum\Wrigley Engine\WE_Startup-main"):
+    shutil.move(rf"C:\Users\{getpass.getuser()}\Charonum\Wrigley Engine\WE_Startup-main\{file}", rf"C:\Users\{getpass.getuser()}\Charonum\Wrigley Engine")
 
-os.remove(rf"C:\Program Files\Charonum\Wrigley Engine\WE_Startup-main.zip")
-os.remove(rf"C:\Program Files\Charonum\Wrigley Engine\WE_Startup-main")
+os.remove(rf"C:\Users\{getpass.getuser()}\Charonum\Wrigley Engine\WE_Startup-main.zip")
+os.remove(rf"C:\Users\{getpass.getuser()}\Charonum\Wrigley Engine\WE_Startup-main")
